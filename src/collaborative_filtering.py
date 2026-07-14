f274483ab98dab9f580edfa52afd8f587b97e8f2
Chris Bouvier <116915293+chbouvi@users.noreply.github.com>
2026-07-14 17:39:16 -0400

    Find movies liked by similar users

--- src/collaborative_filtering.py
+++ src/collaborative_filtering.py
@@ -3,7 +3,7 @@ import pandas as pd
 df_ratings = pd.read_csv("data/ml-latest-small/ratings.csv")
 df_movies = pd.read_csv("data/ml-latest-small/movies.csv")
 
-def find_users_who_like_movie(movie_title, min_rating=4.0):
+def find_users_who_like_movie(movie_title, min_rating):
     movie_row = df_movies[df_movies["title"] == movie_title]
 
     if movie_row.empty:
@@ -17,11 +17,24 @@ def find_users_who_like_movie(movie_title, min_rating=4.0):
         (df_ratings["rating"] >= min_rating)
     ]
 
-    return liked_ratings["userId"].unique()
+    return liked_ratings["userId"].unique(), movie_id
 
-users = find_users_who_like_movie("Toy Story (1995)")
+min_rating = 4.0
+users, movie_id = find_users_who_like_movie("Toy Story (1995)", min_rating)
+
+similar_user_ratings = df_ratings[
+    (df_ratings["userId"].isin(users)) &
+    (df_ratings["rating"] >= min_rating) & 
+    (df_ratings["movieId"] != movie_id)
+]
 
 print(users)
 print(f"Number of users who liked Toy Story: {len(users)}")
+
+print(similar_user_ratings.head())
+print(len(similar_user_ratings))
+
+top_movie_ids = similar_user_ratings["movieId"].value_counts().head(10)
+print(top_movie_ids)
 
 
