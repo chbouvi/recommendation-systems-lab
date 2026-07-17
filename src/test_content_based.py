094f109109f58797fc1d11616691f43c7167fce5
Chris Bouvier <116915293+chbouvi@users.noreply.github.com>
2026-07-17 19:35:52 -0400

    Check content recommender columns

--- src/test_content_based.py
+++ src/test_content_based.py
@@ -13,3 +13,20 @@ def test_original_movie_excluded():
     result = recommend_similar_movies(movie_title)
 
     assert movie_title not in result["title"].values
+
+def test_expected_columns():
+    movie_title = "Toy Story (1995)"
+
+    result = recommend_similar_movies(movie_title)
+
+    expected_set = {
+        "title",
+        "similarity_score",
+        "average_rating",
+        "rating_count",
+        "shared_genres"
+    }
+
+    result_set = set(result.columns)
+
+    assert expected_set.issubset(result_set)
