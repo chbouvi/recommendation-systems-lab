26f394173ad8708fbf5ec8cba3e85554846f969e
Chris Bouvier <116915293+chbouvi@users.noreply.github.com>
2026-07-18 18:16:58 -0400

    Add recall at k metric

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,9 +1,20 @@
 def precision_at_k(recommended_ids, relevant_ids, k):
+    if k == 0:
+        return 0
+    
     k_recommended = recommended_ids[:k]
 
     relevant_in_k = len(set(k_recommended).intersection(set(relevant_ids)))
 
     return relevant_in_k / k
 
+def recall_at_k(recommended_ids, relevant_ids, k):
+    k_recommended = recommended_ids[:k]
 
+    if len(relevant_ids) == 0:
+        return 0
+
+    relevant_in_k = len(set(k_recommended).intersection(set(relevant_ids)))
+    
+    return relevant_in_k / len(relevant_ids)
 
--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -1,6 +1,7 @@
-from evaluation import precision_at_k
+import pytest
+from evaluation import precision_at_k, recall_at_k
 
-def test_precision_at_k():
+def test_valid_precision_at_k():
     recommended_ids = [1, 2, 3, 4, 5, 6]
     relevant_ids = [2, 4, 8]
     k = 5
@@ -9,3 +10,29 @@ def test_precision_at_k():
 
     assert result == 0.4
 
+def test_precision_at_k_zero_k():
+    recommended_ids = [1, 2, 3, 4, 5, 6]
+    relevant_ids = [2, 4, 8]
+    k = 0
+
+    result = precision_at_k(recommended_ids, relevant_ids, k)
+
+    assert result == 0
+
+def test_valid_recall_at_k():
+    recommended_ids = [1, 2, 3, 4, 5, 6]
+    relevant_ids = [2, 4, 8]
+    k = 5
+
+    result = recall_at_k(recommended_ids, relevant_ids, k)
+
+    assert result == pytest.approx(2/3)
+
+def test_recall_at_k_empty_relevant_ids():
+    recommended_ids = [1, 2, 3, 4, 5, 6]
+    relevant_ids = []
+    k = 5
+
+    result = recall_at_k(recommended_ids, relevant_ids, k)
+
+    assert result == 0
