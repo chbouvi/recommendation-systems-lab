236fb52ff35102ff946d65a083d3879542136678
Chris Bouvier <116915293+chbouvi@users.noreply.github.com>
2026-07-19 15:08:12 -0400

    Add liked movies helper

---
+++ src/evaluate_recommenders.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df_ratings = pd.read_csv("data/ml-latest-small/ratings.csv")
+
+def get_relevant_movies_for_user(user_id, min_rating=4.0):
+    ratings_for_user = df_ratings[
+        (df_ratings["userId"] == user_id) &
+        (df_ratings["rating"] >= min_rating)
+    ]
+
+    return ratings_for_user["movieId"].to_list()
+
+result = get_relevant_movies_for_user(1)
+
+print(f"User 1 liked {len(result)} movies")
+print(result)
