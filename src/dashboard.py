f61b9cfa23391cd46de2b57e70092b2fd17edaf7
Chris Bouvier <116915293+chbouvi@users.noreply.github.com>
2026-08-02 17:18:42 -0400

    Add dashboard metric selector on sidebar

--- src/dashboard.py
+++ src/dashboard.py
@@ -6,6 +6,36 @@ outputs_df = pd.read_csv("outputs/evaluation_summary.csv")
 
 st.title("Recommendation Systems Lab")
 
+st.sidebar.header("Choose Metric")
+
+metrics_list = [
+    "Precision",
+    "Recall",
+    "Hit Rate"
+]
+
+default = "hit_rate"
+
+select_metric = st.sidebar.selectbox(
+    "Metric ",
+    metrics_list
+)
+
+
+select_metric = select_metric.strip()
+
+if select_metric == "Precision":
+    final_metric = "precision"
+
+elif select_metric == "Recall":
+    final_metric = "recall"
+
+elif select_metric == "Hit Rate":
+    final_metric = "hit_rate"
+
+else:
+    final_metric = default
+
 st.dataframe(outputs_df, hide_index=True)
 
 outputs_df["k_label"] = outputs_df["k"].astype(str)
@@ -13,15 +43,15 @@ outputs_df["k_label"] = outputs_df["k"].astype(str)
 fig1 = px.bar(
     outputs_df,
     x="k_label",
-    y="hit_rate",
+    y=final_metric,
     color="method",
-    title="Hit Rate per K",
+    title=f"{select_metric} per K",
     barmode="group"
 )
 
 fig1.update_layout(
     xaxis_title="K Value",
-    yaxis_title="Hit Rate",
+    yaxis_title=select_metric,
     xaxis_type="category"
 )
 
