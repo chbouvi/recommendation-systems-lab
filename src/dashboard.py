cfc0ce4abe46677abee8b1b3cf5e9eede3b74a56
Chris Bouvier <116915293+chbouvi@users.noreply.github.com>
2026-08-04 19:39:09 -0400

    Add dashboard summary metrics

--- src/dashboard.py
+++ src/dashboard.py
@@ -40,6 +40,34 @@ st.subheader("Evaluation Results")
 st.dataframe(outputs_df, hide_index=True)
 st.caption("Results are averaged across 100 users and 50 trials per user.")
 
+highest_metric_by_method = outputs_df.groupby("method")[final_metric].max()
+best_method = highest_metric_by_method.idxmax()
+
+if best_method == "collaborative":
+    best_method = "Collaborative filtering"
+elif best_method == "content":
+    best_method = "Content-based"
+
+highest_hit_rate = outputs_df.groupby("k")["hit_rate"].max()
+best_hit_rate = highest_hit_rate[20]
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.metric("Users evaluated", outputs_df["num_users"][0])
+
+with col2:
+    st.metric("Trials per user", outputs_df["trials_per_user"][0])
+
+col3, col4 = st.columns(2)
+
+with col3:
+    st.metric(f"Best method for {select_metric}", best_method)
+
+with col4:
+    st.metric("Best Hit Rate@20", best_hit_rate)
+
+
 outputs_df["k_label"] = outputs_df["k"].astype(str)
 
 fig1 = px.bar(
