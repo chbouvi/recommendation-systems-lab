58f6b296317403c862561052b7c942c2da60530b
Chris Bouvier <116915293+chbouvi@users.noreply.github.com>
2026-08-08 15:18:16 -0400

    Update dashboard seed movie filtering tests

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -1,30 +1,37 @@
 import pandas as pd
 from dashboard import get_valid_seed_movie_titles
 
-df_movies = pd.read_csv("data/ml-latest-small/movies.csv")
-df_ratings = pd.read_csv("data/ml-latest-small/ratings.csv")
+fake_df_movies = pd.DataFrame({
+    "movieId": [1, 2, 3],
+    "title": ["A", "B", "C"],
+    "genres": ["Adventure", "Thriller", "Action"],
+})
+
+fake_df_ratings = pd.DataFrame({
+    "userId": list(range(1, 30)),
+    "movieId": (
+        [1] * 10 +
+        [2] * 9 +
+        [3] * 10
+    ),
+    "rating": (
+        [4.0] * 10 +
+        [4.0] * 9 +
+        [3.5] * 10
+    ),
+})
 
 def test_valid_seed_movie_titles():
-    fake_df_movies = pd.DataFrame({
-        "movieId": [1, 2, 3],
-        "title": ["A", "B", "C"],
-        "genres": ["Adventure", "Thriller", "Action"],
-    })
-
-    fake_df_ratings = pd.DataFrame({
-        "userId": list(range(1, 25)),
-        "movieId": (
-            [1] * 10 +
-            [2] * 9 +
-            [3] * 5
-        ),
-        "rating": (
-            [4.0] * 10 +
-            [4.0] * 9 +
-            [3.5] * 5
-        ),
-    })
-
     movie_titles = get_valid_seed_movie_titles(fake_df_movies, fake_df_ratings)
 
     assert movie_titles == ["A"]
+
+def test_valid_seed_movie_titles_lower_min_rating():
+    movie_titles = get_valid_seed_movie_titles(fake_df_movies, fake_df_ratings, min_rating=3.5)
+
+    assert movie_titles == ["A", "C"]
+
+def test_valid_seed_movie_titles_lower_min_rating_count():
+    movie_titles = get_valid_seed_movie_titles(fake_df_movies, fake_df_ratings, min_rating_count=9)
+
+    assert movie_titles == ["A", "B"]
